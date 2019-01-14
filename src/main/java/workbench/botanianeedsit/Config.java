c0eade93f655b953c83adb109a01ba6ef0ef1a22
Workbench61 <workbench.mods@gmail.com>
2019-01-14 15:16:03 +0700

    Added config sync and some small changes

--- src/main/java/workbench/botanianeedsit/Config.java
+++ src/main/java/workbench/botanianeedsit/Config.java
@@ -4,10 +4,22 @@
  */
 package workbench.botanianeedsit;
 
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import workbench.botanianeedsit.lib.Lib;
 
 @net.minecraftforge.common.config.Config(modid = Lib.General.MOD_ID)
+@Mod.EventBusSubscriber
 public class Config {
     @net.minecraftforge.common.config.Config.RequiresMcRestart
+    @net.minecraftforge.common.config.Config.LangKey("config.allowCapacitors.name")
+    @net.minecraftforge.common.config.Config.Comment("Allow you to craft Manacapacitors")
     public static boolean allowCapacitors = true;
+
+    @SubscribeEvent
+    public static void onConfigChanged(ConfigChangedEvent event) {
+        ConfigManager.sync(Lib.General.MOD_ID, net.minecraftforge.common.config.Config.Type.INSTANCE);
+    }
 }
